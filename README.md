2cdeb5d1d5afe2ec0e4722e3ec4a59f13f09fc43
Mohamed Heiba <mohamed.adel.heiba@gmail.com>
2023-07-17 11:23:57 +0400

    Added .gitignore, Dockerfile, main.py, README.md, and requirements.txt

---
+++ README.md
@@ -0,0 +1,3 @@
+# Subnet Calculator
+
+This is a simple python app that translates a given subnet to its available IP addresses. Running on Docker, exposed via nginx.
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+# This is a simple python app that translates a given subnet to its available IP addresses. Running on Docker,
+# exposed via nginx.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, request
+from ipaddress import ip_network
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route('/subnet')
+def subnet_to_ips():
+    subnet = request.args.get('subnet')
+    ip_addresses = [str(ip) for ip in ip_network(subnet).hosts()]
+    return {
+        "subnet": subnet,
+        "available_ips": ip_addresses
+    }
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
